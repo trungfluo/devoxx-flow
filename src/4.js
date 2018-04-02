c91906656dc37b42f06ffffe0886ccd1660cd580
Trung Nguyen <trung.nguyen@fluo.com>
2018-04-02 20:50:23 +0200

    feat(example): adjust examples

--- src/4.js
+++ src/4.js
@@ -1,14 +1,3 @@
-type Collection = {
-  [vehicle: string]: number | null,
-};
+const name: ?string = 'Paul';
 
-const collection: Collection = {
-  ferrari: 100000,
-  mercedes: 40000,
-  clio: null,
-};
-
-if (collection.ferrari) {
-  console.log(collection);
-  console.log(collection.ferrari.toFixed(1));
-}
+console.log(name.length);
